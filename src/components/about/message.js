af73c9cf713c9f1e823a1c38fd1b9c6fbbbed470
justin <jow@unc.edu>
2025-01-02 17:18:06 -0500

    updae

--- src/components/about/message.js
+++ src/components/about/message.js
@@ -1,13 +1,13 @@
 export const name = "Kelvin";
 export const school = "University of North Carolina at Chapel Hill";
-const thingsIDo = ['Machine Learning', 'Frontend Development', 'Backend Development', 'API Development']
+const thingsIDo = ['Frontend Development', 'Backend Development', 'API Development']
 
 export const message = {
     hello: `Hello👋! My name is `,
     myNameIs: '',
     message: `
         I'm a student at the ${school} 
-        pursuing a Bachelor of Science in Computer Science.
+        pursuing a Bachelor of Science in Information & Computer Science.
         I love to work collaboratively to innovate, engineer and build!
     `,
     whatIDo: thingsIDo
